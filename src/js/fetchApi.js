21cd5c07c7c7c1e9c9c40de2e73a93b3c7884c04
Bigtoxa766 <bigtoxa766@gmail.com>
2023-12-24 02:19:39 +0200

    dependencies,caths

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,53 +1,68 @@
-export function fetchCategories() {
-  const CATEGORY_URL = 'https://food-boutique.b.goit.study/api/products/categories';
-  
-  return fetch(`${CATEGORY_URL}`)
-    .then(resp => {
-      if (!resp.ok) {
-      throw new Error(resp.statusText)
-      }
-      return resp.json()
-  })
-};
+import axios from "axios";
+import Notiflix from 'notiflix';
 
 export class FetchProducts {
   BASE_URL = 'https://food-boutique.b.goit.study/api/products';
   page = 1;
   perPage = 9;
   query = null;
+  inputQuery = null;
 
-  getProducts() {
+  async  getCategories() {
+  const CATEGORY_URL = 'https://food-boutique.b.goit.study/api/products/categories';
+  
+  try {
+    const resp =await axios(`${CATEGORY_URL}`)
+  return await resp.data;
+  } catch (err) {
+    Notiflix.Notify.failure(err)
+  }
+};
+
+  async getProducts() {
 
     const params = new URLSearchParams({
       page: this.page,
       limit: this.perPage,
-     
     });
 
-    return fetch(`${this.BASE_URL}?${params}`)
-      .then(resp => {
-      if (!resp.ok) {
-      throw new Error(resp.statusText)
-      }
-      return resp.json();
-    })
-  } 
+    try {
+      const resp = await axios(`${this.BASE_URL}?${params}`)
+      return await resp.data;
+    } catch (err) {
+      Notiflix.Notify.failure(err)
+    }
+  }
 
-  getProductsByCategory() {
+  async getProductsByCategory() {
     const params = new URLSearchParams({
       page: this.page,
       limit: this.perPage,
       category: this.query,
     });
 
-    return fetch(`${this.BASE_URL}?${params}`)
-      .then(resp => {
-        
-      if (!resp.ok) {
-      throw new Error(resp.statusText)
-      }
-      return resp.json();
-    })
+    try {
+      const resp = await axios(`${this.BASE_URL}?${params}`)
+      return await resp.data;
+    } catch (err) {
+      Notiflix.Notify.failure(err)
+    }
+  };
+
+  async getProductsBySearch() {
+    const params = new URLSearchParams({
+      page: this.page,
+      limit: this.perPage,
+      keyword: this.inputQuery,
+    });
+
+    try {
+      const resp = await axios(`${this.BASE_URL}?${params}`)
+      return await resp.data;
+    } catch (err) {
+      Notiflix.Notify.failure(err)
+    }
   }
+};
   
-}
+
--- src/js/renderProductList.js
+++ src/js/renderProductList.js
@@ -1,11 +1,41 @@
-import { fetchCategories, FetchProducts } from "./fetchApi";
+import { FetchProducts } from "./fetchApi";
+import Notiflix from 'notiflix';
 
-const categoriesEl = document.querySelector('.js-category')
-const productsList = document.querySelector('.card-container')
+const categoriesEl = document.querySelector('.js-category');
+const productsList = document.querySelector('.card-container');
+const searchFormEl = document.querySelector('.js-search-form');
 
 const fetchProducts = new FetchProducts();
 
 categoriesEl.addEventListener('change', onSelect);
+searchFormEl.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  productsList.innerHTML = '';
+
+  fetchProducts.inputQuery = e.target.elements.searchQuery.value.trim();
+
+  fetchProducts.getProductsBySearch()
+    .then(({ results }) => {
+
+      if (results.length === 0) {
+         searchFormEl.reset();
+        
+        window.alert('Oops test')
+      }
+      
+      return results.map(item => {
+     
+    productsList.insertAdjacentHTML('beforeend', handleMarkup(item))
+  })})
+  .catch(err => {
+      if (!err) {
+        return
+      }
+      window.alert('Oops test')
+    });
+}
 
 function handleMarkup(prop) {
   const { category, img, name, popularity, price, size } = prop;
@@ -30,23 +60,41 @@ function onSelect(e) {
   
   fetchProducts.getProductsByCategory()
     .then(({ results }) => results.map(item => {
-    productsList.insertAdjacentHTML('beforeend', handleMarkup(item))
-  }))
+      productsList.insertAdjacentHTML('beforeend', handleMarkup(item))
+    }))
+    .catch(err => {
+      if (!err) {
+        return
+      }
+      window.alert('Oops test')
+    });
 }
-
-fetchCategories().then(data => {
+    
+  fetchProducts.getCategories()
+  .then(data =>
   data.map(item => {
     
     const categoriesMurkup = `<option value="${item}">${item}</option>`;
     return categoriesEl.insertAdjacentHTML('beforeend', categoriesMurkup)
+  }))
+   .catch(err => {
+    if (!err) {
+      return
+    }
+   window.alert('Oops test')
   })
-});
 
 fetchProducts.getProducts()
-  .then(({results}) => {
+  .then(({ results }) => {
     results.map(item => {
       
       productsList.insertAdjacentHTML('beforeend', handleMarkup(item))
     })
   })
+   .catch(err => {
+    if (!err) {
+      return
+    }
+   window.alert('Oops test')
+  });
 
