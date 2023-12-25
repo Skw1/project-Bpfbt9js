8ca25a3ffd3dd5ee0a25cccf21b5782e24757492
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2023-12-25 23:23:47 +0200

    fix bagy

--- src/js/popularProducts.js
+++ src/js/popularProducts.js
@@ -1,22 +1,23 @@
-import axios from "axios";
-export function fetchBreeds(){
-    console.log("hello !");
-    const BASE_URL= "https://food-boutique.b.goit.study/api/products/popular";
+import axios from 'axios';
+export function fetchBreeds() {
+  console.log('hello !');
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products/popular';
 
-  return axios.get(BASE_URL)
-  .then(response =>
-    {
-      return response.data
-  }
-    )
-      .catch(error => {
+  return axios
+    .get(BASE_URL)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
       // console.error(error);
       throw new Error(error);
-  });
-  };
+    });
+}
 //   ---------------------------------------------------------
-export function createMarcup (arr) {
-    return arr.map(({id,img,name,category,size,popularity})=>` 
+export function createMarcup(arr) {
+  return arr
+    .map(
+      ({ id, img, name, category, size, popularity }) => ` 
 
         <li class ="cards-item">
             <div class="photo-card">
@@ -29,10 +30,10 @@ export function createMarcup (arr) {
                         Category:<span class="info-span">${category}</span>
                     </p>
                     <wraper class="info-wraper">
-                        <p class="info-text">
+                        <p class="info-text ">
                             Size:<span class="info-span">${size}</span>
                         </p>
-                        <p class="info-text">
+                        <p class="info-text" >
                             Popularity:<span class="info-span">${popularity}</span>
                         </p>
                     </wraper>
@@ -45,27 +46,25 @@ export function createMarcup (arr) {
                 <div class="check-btn"><svg class="check-icon-svg" width="12" height="12">
                         <use href="./img/icons.svg#check-mark-icon"></use>
                     </svg></div>
-
-               </li>    
-               
-    
-
-`).join("")
+               </li> 
+`
+    )
+    .join('');
 }
 // ---------------------------------------------------------------------
-const containerProducts = document.querySelector(".cards-list")
+const containerProducts = document.querySelector('.cards-list');
 // import { fetchBreeds } from "./fetchApi";
 // import { createMarcup } from "./refs";
 export async function render() {
-    try {
-      const data = await fetchBreeds();
-      console.log('render', data);
-      containerProducts.insertAdjacentHTML('beforeend', createMarcup(data));
-           return await data;
-    } catch (error) {
-      console.log(error.message);
-    }
+  try {
+    const data = await fetchBreeds();
+    console.log('render', data);
+    containerProducts.insertAdjacentHTML('beforeend', createMarcup(data));
+    return await data;
+  } catch (error) {
+    console.log(error.message);
   }
+}
 // ----------------------add main.js-------------------------
 // import { render } from "./js/popularProducts";
 // render ()
