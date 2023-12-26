fb2bba6f3cdcb550eefe39ef60378f419cc7a83b
Skw1 <98914210+Skw1@users.noreply.github.com>
2023-12-26 18:30:50 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@63c06482a091ceb8fa83988eda957510c5114432 🚀

--- assets/main-8146b6bd.js
+++ assets/main-8146b6bd.js
@@ -25,5 +25,5 @@ import{a}from"./vendor-a61d8330.js";(function(){const t=document.createElement("
               </div>
             </div>
          </li>
-        `)}),t.discountList.insertAdjacentHTML("beforeend",l.join(""))}function m(e){let t=d.loadCart(),s=!0;"products"in t&&t.products.forEach(r=>{if(r.productId===e){s=!1;return}}),s&&("products"in t?t.products.push({productId:e,amount:1}):t={email:"",products:[{productId:e,amount:1}]},d.saveCart(t))}function I(e,t){const s=new u;s[`${t}${e}`].href.baseVal===`${f}`&&(s[`${t}${e}`].href.baseVal=`${p}`)}function w(e){e.target.classList.contains("discount-buy")?(m(e.target.dataset.productid),I(e.target.dataset.productid,"discountIcon")):e.target.classList.contains("discount-show")}b();const y=new u;y.discountList.addEventListener("click",w);
-//# sourceMappingURL=main-5f4b82d8.js.map
+        `)}),t.discountList.insertAdjacentHTML("beforeend",l.join(""))}function m(e){let t=d.loadCart(),s=!0;"products"in t&&t.products.forEach(r=>{if(r.productId===e){s=!1;return}}),s&&("products"in t?t.products.push({productId:e,amount:1}):t={email:"",products:[{productId:e,amount:1}]},d.saveCart(t))}function I(e,t){const s=new u;s[`${t}${e}`].href.baseVal===`${f}`&&(s[`${t}${e}`].href.baseVal=`${p}`)}function y(e){e.target.classList.contains("discount-buy")?(m(e.target.dataset.productid),I(e.target.dataset.productid,"discountIcon")):e.target.classList.contains("discount-show")}b();const w=new u;w.discountList.addEventListener("click",y);
+//# sourceMappingURL=main-8146b6bd.js.map
