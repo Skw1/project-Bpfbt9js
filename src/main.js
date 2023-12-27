2841c6f2fe2f46c54963e7e9f91da25b73a69a87
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-27 22:05:55 +0100

    main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import refsAPI from './js/refs.js';
 import { drawDiscount, discountOnClick } from './js/discount.js';
-import { onCardClick, productsList, renderCards } from './js/renderProductList.js';
+
 
 // Test RenderProuctList
 
@@ -16,8 +16,3 @@ frontEnd.discountList.addEventListener('click', discountOnClick);
 
 
 
-// Render Cards
-renderCards()
-
-// Add event listener 
-productsList.addEventListener('click', onCardClick);
