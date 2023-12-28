09e46811c65dc2d3f870b793415787b4f2a374a3
Artem Hordiyevskyy <artem2500@gmail.com>
2023-12-28 23:51:09 +0200

    add header cart number

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import refsAPI from './js/refs.js';
 const frontEnd = new refsAPI();
-import { drawDiscount, discountOnClick } from './js/discount.js';
-import { renderPopular,handleModall } from './js/popularProducts.js';
-import { form } from './js/footer-modal.js';
-import { postEmail } from './js/footer-modal.js';
-
+import {
+  drawDiscount,
+  discountOnClick,
+  drawHeaderCartNumber,
+} from './js/discount.js';
 
 //  RenderProuctList
 //import {onCardClick, renderCards, renderSearchedCards, productsList} from './js/renderProductList.js'
@@ -12,20 +12,31 @@ import { postEmail } from './js/footer-modal.js';
 //productsList.addEventListener('click', onCardClick)
 
 //Modal footer
+
+import { form } from './js/footer-modal.js';
+import { postEmail } from './js/footer-modal.js';
+
 form.addEventListener('submit', postEmail);
 
+//Popular products imports
 
-//Popular products 
+import { renderPopular, handleModall } from './js/popularProducts.js';
 
 renderPopular();
 
-frontEnd.PopularList.addEventListener('click', handleModall)
-
-
 //draw discount products
 drawDiscount();
+drawHeaderCartNumber();
+
+//get DOM tree
+// const frontEnd = new refsAPI();
 
 //add event listener for discount products
 frontEnd.discountList.addEventListener('click', discountOnClick);
 
+// Modal
+//import { renderPopular,handleModall } from './js/popularProducts.js';
 
+//renderPopular()
+
+//frontEnd.PopularList.addEventListener('click', handleModall)
