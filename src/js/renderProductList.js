713e1e43fbdb14ff96ace76dd8beb09089f6969d
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-28 12:45:55 +0100

    Update renderProductList.js

--- src/js/renderProductList.js
+++ src/js/renderProductList.js
@@ -1,10 +1,12 @@
-
 import fetchAPI from './fetchApi.js';
+
 import localStorageApi from './localStorageApi.js';
 
-import discountIcon from './img/icons.svg#discount-icon';
-import cartIcon from './img/icons.svg#discount-cart';
-import checkedIcon from './img/icons.svg#discount-checked';
+
+import discountIcon from '../img/icons.svg#discount-icon';  
+import cartIcon from '../img/icons.svg#discount-cart';
+
+import checkedIcon from '../img/icons.svg#discount-checked';
 
 const productsList = document.querySelector('.product-card-list');
 
@@ -15,93 +17,82 @@ function onCardClick(e) {
 
   if (e.target.classList.contains('product-card-item') || e.target !== e.currentTarget) {
     if (e.target.nodeName === "use" || e.target.nodeName === "BUTTON" || e.target.nodeName === "svg") {
-      console.log('Buy prod', cardId)
-     
-   return
+      console.log('Buy prod', cardId);
+      return;
     }
-    
-    console.log('Open modal', cardId)
-    
-    return
+
+    console.log('Open modal', cardId);
+    return;
   }
-  
 }
+
 function handleMarkup(data) {
   const { category, img, name, popularity, price, size, _id, is10PercentOff } = data;
-  
-  const markup = `
-  <li class="product-card-item discount-list-item" id=${_id} data-discount="${is10PercentOff}">
 
-  <svg class="discount-icon" id=${_id}>
-   <use href="icon.svg#discount-icon"></use>
- </svg>
-       <div class="product-card-wrapper">
+  const markup = `
+    <li class="product-card-item discount-list-item" id="${_id}" data-discount="${is10PercentOff}">
+      <svg class="discount-icon" id="${_id}">
+        <use href="${discountIcon}"></use>
+      </svg>
+      <div class="product-card-wrapper">
         <div class="card-img-wrapper"><img src="${img}" alt="${name}" class="product-card-image"></div>
         <h2 class="product-card-name">${name}</h2>
         <div class="product-card-info">
-            <p class="info-text">
-                Category:<span class="info-span">${category}</span>
-            </p>
-            <p class="info-text">
-                Size:<span class="info-span">${size}</span>
-            </p>
-            <p class="info-text">
-                Popularity:<span class="info-span">${popularity}</span>
-            </p>
+          <p class="info-text">
+            Category:<span class="info-span">${category}</span>
+          </p>
+          <p class="info-text">
+            Size:<span class="info-span">${size}</span>
+          </p>
+          <p class="info-text">
+            Popularity:<span class="info-span">${popularity}</span>
+          </p>
         </div>
         <div class="product-card-bottom">
-        <p class="product-curd-price">$${price}</p>
-        <button class="card_buy-btn">
-          <svg class="card_buy-logo-icon">
-                <use href="./img/icons.svg#shopping-cart-icon"></use>
-          </svg>
-        </button>
+          <p class="product-curd-price">$${price}</p>
+          <button class="card_buy-btn">
+            <svg class="card_buy-logo-icon">
+              <use href="${cartIcon}"></use>
+            </svg>
+          </button>
         </div>
-        </div>
+      </div>
     </li>
   `;
 
   return markup;
-  
-};
+}
 
 function renderCards() {
   productListApi.results.map(item => {
     return productsList.insertAdjacentHTML('beforeend', handleMarkup(item));
-  }
-  )
-};
+  });
+}
 
 function shownDiscIcon(arr) {
   arr.map(el => {
-    const icon = document.querySelector('.discount-icon')
-    
-    console.log(icon)
-    
-  })
-};
-// shownDiscIcon(productListApi.results)
+    const icons = document.querySelectorAll('.discount-icon');
 
-
+    icons.forEach(icon => {
+      console.log(icon);
+    });
+  });
+}
 
 function renderSearchedCards(category, search) {
   productsList.innerHTML = '';
 
   if (category) {
-    return category.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)))
+    return category.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)));
   }
 
   if (search) {
     if (search.length === 0) {
-        window.alert('Oops test')
-      }
-    return search.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)))
+      window.alert('Oops test');
+    }
+
+    return search.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)));
   }
-};
+}
 
-export{onCardClick, renderCards, renderSearchedCards, productsList}
-
-
-
-
-
+export { onCardClick, renderCards, renderSearchedCards, productsList };
