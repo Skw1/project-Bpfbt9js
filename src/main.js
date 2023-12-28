009f89aa3248f31bcb9b949290368148ff510d9a
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-28 12:19:00 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import refsAPI from './js/refs.js';
 import { drawDiscount, discountOnClick } from './js/discount.js';
 
-//draw discount products
-drawDiscount();
-
-//get DOM tree
-const frontEnd = new refsAPI();
-
-//add event listener for discount products
-frontEnd.discountList.addEventListener('click', discountOnClick);
-
 //Modal footer
 
 import { form } from './js/footer-modal.js';
@@ -21,10 +12,21 @@ form.addEventListener('submit', postEmail);
 
 import { renderPopular,handleModall } from './js/popularProducts.js';
 renderPopular()
- frontEnd.PopularList.addEventListener('click', handleModall);
+ frontEnd.PopularList.addEventListener('click', handleModall)
 
+
+
+
+//draw discount products
+drawDiscount();
+
+//get DOM tree
+const frontEnd = new refsAPI();
+
+//add event listener for discount products
+frontEnd.discountList.addEventListener('click', discountOnClick);
 
 //  RenderProuctList
 import { onCardClick, productsList, renderCards } from './js/renderProductList.js';
 renderCards()
-productsList.addEventListener('click', onCardClick);
+frontEnd.productsList.addEventListener('click', onCardClick);
