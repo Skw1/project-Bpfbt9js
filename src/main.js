caedcc76d1c35551683114902b3aff1a241203f5
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-28 23:11:37 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import refsAPI from './js/refs.js';
 const frontEnd = new refsAPI();
 import {drawDiscount, discountOnClick, drawHeaderCartNumber} from './js/discount.js';
 
-import { drawDiscount, discountOnClick } from './js/discount.js';
 import { renderPopular,handleModall } from './js/popularProducts.js';
 import { form } from './js/footer-modal.js';
 import { postEmail } from './js/footer-modal.js';
