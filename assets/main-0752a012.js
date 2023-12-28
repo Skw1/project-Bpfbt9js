0410bff24a0c0246a4bd3aa90ed2bf9b3ee38be5
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-12-28 20:22:45 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@23ab830c06f226ea19c4b63dc627d004bb67248b 🚀

--- assets/main-0752a012.js
+++ assets/main-0752a012.js
@@ -130,4 +130,4 @@ import{a as u,b as B}from"./vendor-0363fca5.js";(function(){const t=document.cre
                
     
 `).join("")}async function X(){try{const o=_;v.PopularList.insertAdjacentHTML("beforeend",Q(o)),y=new p;let t=d.loadCart();const s=[];"products"in t&&(t=t.products,t.forEach(e=>s.push(e.productId)));const a=[];return _.forEach(e=>{s.includes(e._id)&&(y[`btn1${e._id}`].style.display="none",y[`check${e._id}`].style.display="flex")}),await o}catch(o){console.log(o.message)}}const Z=new p;U.addEventListener("submit",Y);G();H();Z.discountList.addEventListener("click",K);
-//# sourceMappingURL=main-d84323cc.js.map
+//# sourceMappingURL=main-0752a012.js.map
