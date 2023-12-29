21d32c50dc5c2dae4e565caaed960125e56eb263
Skw1 <98914210+Skw1@users.noreply.github.com>
2023-12-29 20:07:58 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@846d86d630ddffe97707959ec63dd8ceacc4d324 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{r as i,l as s,f as p,c as C,d as u,a as g,p as y}from"./assets/footer-modal-3bb25060.js";import"./assets/vendor-7ba42fd8.js";async function d(){let n=!0;const a=new i;a.bigCartNumber.textContent="CART (0)";var r=[],c=0;const e=s.loadCart();if("products"in e){const o=await Promise.all(e.products.map(t=>p.product(t.productId)));o.forEach(t=>{n=!1,c=c+t.price*e.products[o.indexOf(t)].amount,r.push(`
+import{r as l,l as n,f as p,c as y,d as u,a as C,p as v}from"./assets/footer-modal-3bb25060.js";import"./assets/vendor-7ba42fd8.js";async function i(){let r=!0;const e=new l;console.log(e),e.bigCartNumber.textContent="CART (0)";var s=[],c=0;const a=n.loadCart();if("products"in a){const o=await Promise.all(a.products.map(t=>p.product(t.productId)));o.forEach(t=>{r=!1,c=c+t.price*a.products[o.indexOf(t)].amount,s.push(`
             
             <li class="cart-product" data-productId="${t._id}">
             <div class="cart-product-img">
@@ -13,7 +13,7 @@ import{r as i,l as s,f as p,c as C,d as u,a as g,p as y}from"./assets/footer-mod
               <h3 class="cart-product-name">${t.name}</h3>
               <button data-productId="${t._id}" type="button" class="cart-product-delete-btn js-cart-prod-del">
               <svg class="cart-icon-close" width="18" height="18">
-                    <use href="${C}"></use>
+                    <use href="${y}"></use>
                     </svg>
               </button>
             </div>
@@ -29,5 +29,5 @@ import{r as i,l as s,f as p,c as C,d as u,a as g,p as y}from"./assets/footer-mod
                   <p class="cart-product-price">$${t.price}</p>
             </div>
           </li>
-          `)}),a.bigCartNumber.textContent="CART ("+e.products.length+")"}n?(a.cartEmptyContainer.style.display="block",a.totalCartSum.textContent="Sum: $0",a.cartContainer.innerHTML=""):(a.cartEmptyContainer.style.display="none",a.cartContainer.innerHTML=r.join(""),a.totalCartSum.textContent="Sum: $"+c.toFixed(2)),"products"in e&&e.products.length&&document.querySelector(".js-cart-prod-del").addEventListener("click",o=>{const t=new i,m=o.target.closest(".js-cart-prod-del").dataset.productid,l=e.products.findIndex(f=>f.productId===m);l!==-1&&e.products.splice(l,1),s.saveCart(e),d(),console.log(t),t.cartEmptyContainer.style.display="block"})}async function v(n){n.preventDefault();const a=new i,r=s.loadCart();if(a.cartEmail.value.trim().length!==0)if("products"in r)if(r.products.length!==0)try{r.email=a.cartEmail.value.trim();const c=await p.order(r);s.deleteCart(),u(),d(),alert(c.message)}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}s.loadCart();u();d();const h=new i;h.buttonCheckout.addEventListener("click",v);g.addEventListener("submit",y);
+          `)}),e.bigCartNumber.textContent="CART ("+a.products.length+")"}r?(e.deleteAllBtn.style.display="none",e.cartEmptyContainer.style.display="block",e.totalCartSum.textContent="Sum: $0",e.cartContainer.innerHTML=""):(e.deleteAllBtn.style.display="block",e.cartEmptyContainer.style.display="none",e.cartContainer.innerHTML=s.join(""),e.totalCartSum.textContent="Sum: $"+c.toFixed(2)),"products"in a&&a.products.length&&document.querySelector(".js-cart-prod-del").addEventListener("click",o=>{const t=new l,f=o.target.closest(".js-cart-prod-del").dataset.productid,d=a.products.findIndex(g=>g.productId===f);d!==-1&&a.products.splice(d,1),n.saveCart(a),i(),console.log(t),t.cartEmptyContainer.style.display="block"})}async function h(r){r.preventDefault();const e=new l,s=n.loadCart();if(e.cartEmail.value.trim().length!==0)if("products"in s)if(s.products.length!==0)try{s.email=e.cartEmail.value.trim();const c=await p.order(s);n.deleteCart(),u(),i(),alert(c.message)}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}u();i();const m=new l;m.buttonCheckout.addEventListener("click",h);C.addEventListener("submit",v);m.deleteAllBtn.addEventListener("click",r=>{console.log(r.target.classList.contains("js-delete-all")),r.target.classList.contains("js-delete-all")&&(n.deleteCart(),i())});
 //# sourceMappingURL=commonHelpers.js.map
