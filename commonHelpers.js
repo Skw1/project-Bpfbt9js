32b0079d38aab9c272fea58793727142210518ea
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-12-29 16:28:58 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@26360343d903e218fa7299e144d107c41720a497 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{r as o,l as n,f as l,c as m,d,a as u,p as C}from"./assets/footer-modal-21aff4b3.js";import"./assets/vendor-7ba42fd8.js";async function p(){let c=!0;const a=new o;a.bigCartNumber.textContent="CART (0)";var e=[],r=0;const s=n.loadCart();if("products"in s){const i=await Promise.all(s.products.map(t=>l.product(t.productId)));i.forEach(t=>{c=!1,r=r+t.price*s.products[i.indexOf(t)].amount,e.push(`
+import{r as i,l as s,f as p,c as C,d as u,a as g,p as y}from"./assets/footer-modal-21aff4b3.js";import"./assets/vendor-7ba42fd8.js";async function d(){let n=!0;const a=new i;a.bigCartNumber.textContent="CART (0)";var r=[],c=0;const e=s.loadCart();if("products"in e){const o=await Promise.all(e.products.map(t=>p.product(t.productId)));o.forEach(t=>{n=!1,c=c+t.price*e.products[o.indexOf(t)].amount,r.push(`
             
             <li class="cart-product" data-productId="${t._id}">
             <div class="cart-product-img">
@@ -11,9 +11,9 @@ import{r as o,l as n,f as l,c as m,d,a as u,p as C}from"./assets/footer-modal-21
             <div class="cart-product-card">
             <div class="cart-product-name-container">
               <h3 class="cart-product-name">${t.name}</h3>
-              <button data-productId="${t._id}" type="button" class="cart-product-delete-btn">
+              <button data-productId="${t._id}" type="button" class="cart-product-delete-btn js-cart-prod-del">
               <svg class="cart-icon-close" width="18" height="18">
-                    <use href="${m}"></use>
+                    <use href="${C}"></use>
                     </svg>
               </button>
             </div>
@@ -29,5 +29,5 @@ import{r as o,l as n,f as l,c as m,d,a as u,p as C}from"./assets/footer-modal-21
                   <p class="cart-product-price">$${t.price}</p>
             </div>
           </li>
-          `)}),a.bigCartNumber.textContent="CART ("+s.products.length+")"}c?(a.cartEmptyContainer.style.display="block",a.totalCartSum.textContent="Sum: $0",a.cartContainer.innerHTML=""):(a.cartEmptyContainer.style.display="none",a.cartContainer.innerHTML=e.join(""),a.totalCartSum.textContent="Sum: $"+r.toFixed(2))}async function f(c){c.preventDefault();const a=new o,e=n.loadCart();if(a.cartEmail.value.trim().length!==0)if("products"in e)if(e.products.length!==0)try{e.email=a.cartEmail.value.trim();const r=await l.order(e);n.deleteCart(),d(),p(),alert(r.message)}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}n.loadCart();d();p();const g=new o;g.buttonCheckout.addEventListener("click",f);u.addEventListener("submit",C);
+          `)}),a.bigCartNumber.textContent="CART ("+e.products.length+")"}n?(a.cartEmptyContainer.style.display="block",a.totalCartSum.textContent="Sum: $0",a.cartContainer.innerHTML=""):(a.cartEmptyContainer.style.display="none",a.cartContainer.innerHTML=r.join(""),a.totalCartSum.textContent="Sum: $"+c.toFixed(2)),"products"in e&&e.products.length&&document.querySelector(".js-cart-prod-del").addEventListener("click",o=>{const t=new i,m=o.target.closest(".js-cart-prod-del").dataset.productid,l=e.products.findIndex(f=>f.productId===m);l!==-1&&e.products.splice(l,1),s.saveCart(e),d(),console.log(t),t.cartEmptyContainer.style.display="block"})}async function v(n){n.preventDefault();const a=new i,r=s.loadCart();if(a.cartEmail.value.trim().length!==0)if("products"in r)if(r.products.length!==0)try{r.email=a.cartEmail.value.trim();const c=await p.order(r);s.deleteCart(),u(),d(),alert(c.message)}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}s.loadCart();u();d();const h=new i;h.buttonCheckout.addEventListener("click",v);g.addEventListener("submit",y);
 //# sourceMappingURL=commonHelpers.js.map
