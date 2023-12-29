02b47dc90fbc5d42e4ada29aa0821a9d1f6ca2f7
Bigtoxa766 <bigtoxa766@gmail.com>
2023-12-29 09:50:55 +0200

    update

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ renderCards();
 
 
 // Add event listener for  Product Cards
-frontEnd.productsList.addEventListener('click', onCardClick);
+document.querySelector('.product_card-list').addEventListener('click', onCardClick);
 
 
 // Draw Popular products 
