dbbc48c13a33704da83e1b7b4483133da783ca06
Skw1 <98914210+Skw1@users.noreply.github.com>
2024-01-01 13:24:08 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@eeeea0fba010f1ea79d87c39e72d4982c7d91e31 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,19 +1,19 @@
-import{r as l,l as c,f as u,c as v,d as m,M as i,a as h,p as C}from"./assets/footer-modal-1f170b31.js";import"./assets/vendor-7ba42fd8.js";const b="/project-Bpfbt9js/assets/tomatto-a27bffcd.png";async function d(){let s=!0;const e=new l;console.log(e),e.bigCartNumber.textContent="CART (0)";var r=[],o=0;const a=c.loadCart();if("products"in a){const n=await Promise.all(a.products.map(t=>u.product(t.productId)));n.forEach(t=>{s=!1,o=o+t.price*a.products[n.indexOf(t)].amount,r.push(`
+import{r as n,l as o,f as u,c as f,d as m,M as i,a as v,p as y}from"./assets/footer-modal-1f170b31.js";import"./assets/vendor-7ba42fd8.js";const g="/project-Bpfbt9js/assets/tomatto-a27bffcd.png";async function d(){let e=!0;const t=new n;t.bigCartNumber.textContent="CART (0)";var s=[],r=0;const c=o.loadCart();if("products"in c){const p=await Promise.all(c.products.map(a=>u.product(a.productId)));p.forEach(a=>{e=!1,r=r+a.price*c.products[p.indexOf(a)].amount,s.push(`
             
-          <li class="cart-product" data-productId="${t._id}">
+          <li class="cart-product" data-productId="${a._id}">
             <div class="cart-product-img">
               <img class="card-img"
-                src="${t.img}"
-                alt="${t.name}"
+                src="${a.img}"
+                alt="${a.name}"
               />
             </div>
 
             <div class="cart-product-card">
               <div class="cart-product-name-container">
-                <h3 class="cart-product-name">${t.name}</h3>
-                <button data-productId="${t._id}" type="button" class="cart-product-delete-btn js-cart-prod-del">
-                <svg class="cart-icon-close" width="18" height="18">
-                    <use href="${v}#close-icon"></use>
+                <h3 class="cart-product-name">${a.name}</h3>
+                <button data-productId="${a._id}" type="button" class="cart-product-delete-btn js-cart-prod-del">
+                <svg class="cart-icon-close js-cart-prod-del" data-productId="${a._id}" width="18" height="18">
+                    <use class="js-cart-prod-del" data-productId="${a._id}" href="${f}#close-icon"></use>
                     </svg>
                 </button>
               </div>
@@ -21,24 +21,24 @@ import{r as l,l as c,f as u,c as v,d as m,M as i,a as h,p as C}from"./assets/foo
                 <div class="cart-product-info">
                   <p class="cart-category-title">
                     Category:
-                    <span class="cart-category-name">${t.category}</span>
+                    <span class="cart-category-name">${a.category}</span>
                   </p>
                   <p class="cart-product-size">
-                    Size: <span class="cart-product-size-value">${t.size}</span>
+                    Size: <span class="cart-product-size-value">${a.size}</span>
                   </p>
                 </div>
                   <div class="cart-product-price" >
-                    <span class="cart-product-span">$${t.price}</span>
+                    <span class="cart-product-span">$${a.price}</span>
                   </div>
                 
             </div>
           </li>
           <hr class=""divider>
-          `)}),e.bigCartNumber.textContent="CART ("+a.products.length+")"}s?(e.deleteAllBtn.style.display="none",e.cartEmptyContainer.style.display="block",e.totalCartSum.textContent="$0",e.cartContainer.innerHTML=""):(e.deleteAllBtn.style.display="block",e.cartEmptyContainer.style.display="none",e.cartContainer.innerHTML=r.join(""),e.totalCartSum.textContent="$"+o.toFixed(2)),"products"in a&&a.products.length&&document.querySelector(".js-cart-prod-del").addEventListener("click",n=>{const t=new l,f=n.target.closest(".js-cart-prod-del").dataset.productid,p=a.products.findIndex(g=>g.productId===f);p!==-1&&a.products.splice(p,1),c.saveCart(a),d(),console.log(t),t.cartEmptyContainer.style.display="block"})}async function E(s){s.preventDefault();const e=new l,r=c.loadCart();if(e.cartEmail.value.trim().length!==0)if("products"in r)if(r.products.length!==0)try{r.email=e.cartEmail.value.trim();const o=await u.order(r);c.deleteCart(),m(),d(),i._modalImg.style.display="none",i._modal.innerHTML=`
+          `)}),t.bigCartNumber.textContent="CART ("+c.products.length+")"}e?(t.deleteAllBtn.style.display="none",t.cartEmptyContainer.style.display="block",t.totalCartSum.textContent="$0",t.cartContainer.innerHTML=""):(t.deleteAllBtn.style.display="block",t.cartEmptyContainer.style.display="none",t.cartContainer.innerHTML=s.join(""),t.totalCartSum.textContent="$"+r.toFixed(2))}async function C(e){e.preventDefault();const t=new n,s=o.loadCart();if(t.cartEmail.value.trim().length!==0)if("products"in s)if(s.products.length!==0)try{s.email=t.cartEmail.value.trim();const r=await u.order(s);o.deleteCart(),m(),d(),i._modalImg.style.display="none",i._modal.innerHTML=`
         <div class="modal-inner">
           <img
             class="modal__food-tomatto"
-            src="${b}"
+            src="${g}"
             alt="Кошик з фруктамі"
           />
           <div class="modal__title--wrap-success">
@@ -53,5 +53,5 @@ import{r as l,l as c,f as u,c as v,d as m,M as i,a as h,p as C}from"./assets/foo
               happiness.
             </p>
           </div>
-        </div>`,i.open()}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}m();d();const y=new l;y.buttonCheckout.addEventListener("click",E);h.addEventListener("submit",C);y.deleteAllBtn.addEventListener("click",s=>{console.log(s.target.classList.contains("js-delete-all")),s.target.classList.contains("js-delete-all")&&(c.deleteCart(),d())});
+        </div>`,i.open()}catch{alert("Check email!")}else alert("empty cart2!");else alert("empty cart1!");else alert("empty email!")}m();d();const l=new n;l.buttonCheckout.addEventListener("click",C);v.addEventListener("submit",y);l.deleteAllBtn.addEventListener("click",e=>{e.target.classList.contains("js-delete-all")&&(o.deleteCart(),d())});l.cartContainer.addEventListener("click",e=>{let t=o.loadCart();if(e.target.classList.contains("js-cart-prod-del")){const s=e.target.dataset.productid,r=t.products.findIndex(c=>c.productId===s);r!==-1&&t.products.splice(r,1),o.saveCart(t),d()}});
 //# sourceMappingURL=commonHelpers.js.map
