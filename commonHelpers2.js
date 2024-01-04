98933b2a5b386e6f2d56fc62613e3a54872caf89
Skw1 <98914210+Skw1@users.noreply.github.com>
2024-01-04 16:50:41 +0000

    Deploying to gh-pages from @ Skw1/project-Bpfbt9js@fda9401aac8bec81c2be37066a2b7a6d2b6b8d2a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,5 +1,5 @@
-import{r as y,b as E,g as N,f as m,l as T,e as S,h as j,d as F,i as V,j as U,a as G,p as R}from"./assets/footer-modal-7f5e354d.js";import{P as Y}from"./assets/vendor-f89adc3e.js";const L=document.querySelector(".error");new y;let i,k,_={limit:null};async function O(e){document.querySelector(".product_card-list").innerHTML="",e.results.map(a=>document.querySelector(".product_card-list").insertAdjacentHTML("beforeend",A(a)))}function K(e){if(e.target.classList.contains("product_card-item")||e.target!==e.currentTarget){let a=setInterval(function(){I(),x()},1e3);if(setTimeout(function(){clearInterval(a)},1e4),e.target.nodeName==="use"||e.target.nodeName==="BUTTON"||e.target.nodeName==="svg"){const t=e.target.closest(".product_card-item").dataset.productid;E(t),i[`btn2${t}`].style.display="none",i[`check1${t}`].style.display="flex";return}N(e,".product_card-item");return}}function A(e){const{category:a,img:t,name:s,popularity:u,price:h,size:b,_id:n,is10PercentOff:D}=e;return D?`
-  <li class="product_card-item js-object" id=${n} data-productId="${n}" >
+import{r as y,b as P,g as x,f as m,l as L,e as $,h as _,d as W,i as z,j as D,a as F,p as V}from"./assets/footer-modal-7f5e354d.js";import{P as U}from"./assets/vendor-f89adc3e.js";const I=document.querySelector(".error");new y;let l,w,b={limit:null};async function E(e){document.querySelector(".product_card-list").innerHTML="",e.results.map(a=>document.querySelector(".product_card-list").insertAdjacentHTML("beforeend",N(a)))}function G(e){if(e.target.classList.contains("product_card-item")||e.target!==e.currentTarget){let a=setInterval(function(){j(),C()},1e3);if(setTimeout(function(){clearInterval(a)},1e4),e.target.nodeName==="use"||e.target.nodeName==="BUTTON"||e.target.nodeName==="svg"){const t=e.target.closest(".product_card-item").dataset.productid;P(t),l[`btn2${t}`].style.display="none",l[`check1${t}`].style.display="flex";return}x(e,".product_card-item");return}}function N(e){const{category:a,img:t,name:s,popularity:n,price:i,size:p,_id:c,is10PercentOff:B}=e;return B?`
+  <li class="product_card-item js-object" id=${c} data-productId="${c}" >
        <div class="product_card-wrapper">
         <div class="card-img-wrapper"><img src="${t}" alt="${s}" class="product_card-image"></div>
         <h2 class="product_card-name">${s}</h2>
@@ -8,28 +8,28 @@ import{r as y,b as E,g as N,f as m,l as T,e as S,h as j,d as F,i as V,j as U,a a
                 Category:<span class="info-span">${a}</span>
             </p>
             <p class="info-text">
-                Size:<span class="info-span">${b}</span>
+                Size:<span class="info-span">${p}</span>
             </p>
             <p class="info-text">
-                Popularity:<span class="info-span">${u}</span>
+                Popularity:<span class="info-span">${n}</span>
             </p>
         </div>
         <div class="product_card-bottom">
-        <p class="product-curd-price">$${h}</p>
-         <button id="${n}" class="card_buy-btn js-btn discount-buy js-object" data-jsname="btn2${n}"  type="button">
+        <p class="product-curd-price">$${i}</p>
+         <button id="${c}" class="card_buy-btn js-btn discount-buy js-object" data-jsname="btn2${c}"  type="button">
                     <svg class="card_buy-icon-svg js-btn "   width="18" height="18">
-                        <use class="js-btn" href="${S}"></use>
+                        <use class="js-btn" href="${$}"></use>
                     </svg>
                 </button>
-                <div id="${n}" class="check js-object" data-jsname="check1${n}" >
+                <div id="${c}" class="check js-object" data-jsname="check1${c}" >
                 <svg  class="check-icon-svg  discount-buy " width="18" height="18">
-                        <use href="${j}"></use>
+                        <use href="${_}"></use>
                     </svg></div>
         </div>
         </div>
     </li>
   `:`
-  <li class="product_card-item js-object" id=${n} data-productId="${n}" >
+  <li class="product_card-item js-object" id=${c} data-productId="${c}" >
 
        <div class="product_card-wrapper">
         <div class="card-img-wrapper"><img src="${t}" alt="${s}" class="product_card-image"></div>
@@ -39,27 +39,27 @@ import{r as y,b as E,g as N,f as m,l as T,e as S,h as j,d as F,i as V,j as U,a a
                 Category:<span class="info-span">${a}</span>
             </p>
             <p class="info-text">
-                Size:<span class="info-span">${b}</span>
+                Size:<span class="info-span">${p}</span>
             </p>
             <p class="info-text">
-                Popularity:<span class="info-span">${u}</span>
+                Popularity:<span class="info-span">${n}</span>
             </p>
         </div>
         <div class="product_card-bottom">
-        <p class="product-curd-price">$${h}</p>
+        <p class="product-curd-price">$${i}</p>
         
-        <button id="${n}" class="card_buy-btn js-btn discount-buy js-object" data-jsname="btn2${n}"  type="button">
+        <button id="${c}" class="card_buy-btn js-btn discount-buy js-object" data-jsname="btn2${c}"  type="button">
                     <svg class="card_buy-icon-svg js-btn "   width="18" height="18">
-                        <use class="js-btn" href="${S}"></use>
+                        <use class="js-btn" href="${$}"></use>
                     </svg>
                 </button>
-                <div id="${n}" class="check js-object" data-jsname="check1${n}" >
+                <div id="${c}" class="check js-object" data-jsname="check1${c}" >
                 <svg  class="check-icon-svg  discount-buy " width="18" height="18">
-                        <use href="${j}"></use>
+                        <use href="${_}"></use>
                     </svg></div>
         </div>
         </div>
-    </li>`}function M(){let e=window.innerWidth||document.documentElement.clientWidth;e>=1440?_={limit:9}:e<1440&&e>768?_={limit:8}:_={limit:6}}async function Q(){M(),k=await m.products(_),k.results.map(e=>document.querySelector(".product_card-list").insertAdjacentHTML("beforeend",A(e))),i=new y,I()}function I(){let e=T.loadCart();const a=[];"products"in e&&(e=e.products,e.forEach(t=>a.push(t.productId))),k.results.forEach(t=>{a.includes(t._id)?(i[`btn2${t._id}`].style.display="none",i[`check1${t._id}`].style.display="flex"):(i[`check1${t._id}`].style.display="none",i[`btn2${t._id}`].style.display="flex")})}let C,P,v,o;async function X(){C=new y,C.PopularList.innerHTML="",P=await m.popular(),ae()}function Z(e){v=e.target.closest(".cards-item").dataset.productid,o[`btn1${v}`].style.display="none",o[`check${v}`].style.display="flex",E(v)}function ee(e){let a=setInterval(function(){x(),I()},1e3);if(setTimeout(function(){clearInterval(a)},1e4),e.target.classList.contains("js-btn")){Z(e);return}if(!e.target.classList.contains("js-btn")){N(e,".cards-item");return}}function te(e){return e.map(({_id:a,img:t,name:s,category:u,size:h,popularity:b})=>` 
+    </li>`}function T(){let e=window.innerWidth||document.documentElement.clientWidth;e>=1440?b={limit:9}:e<1440&&e>768?b={limit:8}:b={limit:6}}async function R(){T(),w=await m.products(b),w.results.map(e=>document.querySelector(".product_card-list").insertAdjacentHTML("beforeend",N(e))),l=new y,j()}function j(){let e=L.loadCart();const a=[];"products"in e&&(e=e.products,e.forEach(t=>a.push(t.productId))),w.results.forEach(t=>{a.includes(t._id)?(l[`btn2${t._id}`].style.display="none",l[`check1${t._id}`].style.display="flex"):(l[`check1${t._id}`].style.display="none",l[`btn2${t._id}`].style.display="flex")})}let S,k,h,d;async function Y(){S=new y,S.PopularList.innerHTML="",k=await m.popular(),Z()}function K(e){h=e.target.closest(".cards-item").dataset.productid,d[`btn1${h}`].style.display="none",d[`check${h}`].style.display="flex",P(h)}function Q(e){let a=setInterval(function(){C(),j()},1e3);if(setTimeout(function(){clearInterval(a)},1e4),e.target.classList.contains("js-btn")){K(e);return}if(!e.target.classList.contains("js-btn")){x(e,".cards-item");return}}function X(e){return e.map(({_id:a,img:t,name:s,category:n,size:i,popularity:p})=>` 
     <li class ="cards-item" data-productid="${a}" >
         <div id ="${a}" class="photo-card">
                     <img class = "cards__image" src="${t}" alt="${s}" loading="lazy" />
@@ -68,27 +68,27 @@ import{r as y,b as E,g as N,f as m,l as T,e as S,h as j,d as F,i as V,j as U,a a
             <div class=" discount-show">
                     <h3 class="info-title">${s}</h3>      
                     <p class="info-text">
-                        Category:<span class="info-span">${u}</span>
+                        Category:<span class="info-span">${n}</span>
                     </p>
                     <wraper class="info-wraper">
                         <p class="info-text">
-                            Size:<span class="info-span">${h}</span>
+                            Size:<span class="info-span">${i}</span>
                         </p>
                         <p class="info-text">
-                            Popularity:<span class="info-span">${b}</span>
+                            Popularity:<span class="info-span">${p}</span>
                         </p>
                     </wraper>
                </div>
                <button id="${a}" class="popular-products-btn js-btn discount-buy js-object" data-jsname="btn1${a}"  type="button">
                     <svg class="basket-icon-svg js-btn "   width="12" height="12">
-                        <use class="js-btn" href="${S}"></use>
+                        <use class="js-btn" href="${$}"></use>
                     </svg>
                 </button>
                 <div id="${a}" class="check-btn js-object js-btn" data-jsname="check${a}" >
                 <svg  class="check-icon-svg  discount-buy  js-btn" width="12" height="12">
-                        <use href="${j}"></use>
+                        <use href="${_}"></use>
                     </svg></div>
                </li>    
                   
-`).join("")}async function ae(){try{const e=P;return C.PopularList.insertAdjacentHTML("beforeend",te(e)),o=new y,x(),await e}catch(e){console.log(e.message)}}function x(){let e=T.loadCart();const a=[];"products"in e&&(e=e.products,e.forEach(t=>a.push(t.productId))),P.forEach(t=>{a.includes(t._id)?(o[`btn1${t._id}`].style.display="none",o[`check${t._id}`].style.display="flex"):(o[`check${t._id}`].style.display="none",o[`btn1${t._id}`].style.display="flex")})}let l=JSON.parse(localStorage.getItem("keyword")),$=JSON.parse(localStorage.getItem("category")),d={};const se=document.getElementById("pagination");function ne(e,a,t){const s={totalItems:e,itemsPerPage:a,visiblePages:5,page:t,centerAlign:!0};new Y(se,s).on("afterMove",ce)}async function ce(e){const a=e.page;var t=JSON.parse(localStorage.getItem("filters"))||{};t.page=a,l==""?l=null:l=JSON.parse(localStorage.getItem("keyword")),l==null?$.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?d={page:t.page}:d={category:$.replace(" ","_").replace("/","&").replace(" ","_"),page:t.page}:$.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?d={keyword:l,page:t.page}:d={category:$.replace(" ","_").replace("/","&").replace(" ","_"),keyword:l,page:t.page},console.log(d);const s=await m.products(d);return O(s)}const p=document.querySelector("#dropdownMenu"),q=document.querySelector("#input-filter"),re=document.querySelector("#filterButt"),f=document.querySelector(".category-content"),ie=document.querySelector(".dropdown-toggle"),w="keyword",H="category";let r=JSON.parse(localStorage.getItem(w)),c=JSON.parse(localStorage.getItem(H)),g={};function J(){(r||c)&&(q.value=r,f.textContent=c)}async function B(){let a=(await m.categories()).map(t=>t.replace("_"," ").replace("&","/").replace("_"," "));a.push("Show all"),c!==null?f.textContent=c:f.textContent="Categories",p.innerHTML="",a.forEach(async t=>{const s=document.createElement("div");s.className="dropdown-item",s.textContent=t,s.onclick=async function(){c=t,localStorage.setItem(H,JSON.stringify(c)),W()},p.appendChild(s)})}async function W(){p.style.display=p.style.display==="flex"?"none":"flex",c!==null?f.textContent=c:f.textContent="Categories",setTimeout(function(){p.style.opacity=p.style.opacity==="1"?"0":"1"},10)}function oe(e){const a=e.target.value;a?localStorage.setItem(w,JSON.stringify(a)):localStorage.setItem(w,JSON.stringify(null))}async function le(e){e.preventDefault(),r==""?r=null:r=JSON.parse(localStorage.getItem(w));let a=window.innerWidth||document.documentElement.clientWidth,t="";a>=1440?t=9:a<1440&&a>768?t=8:t=6,r==null?c.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?g={limit:t}:g={category:c.replace(" ","_").replace("/","&").replace(" ","_"),limit:t}:c.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?g={keyword:r,limit:t}:g={category:c.replace(" ","_").replace("/","&").replace(" ","_"),keyword:r,limit:t};const s=await m.products(g);return s.results.length===0?L.style.display="flex":L.style.display="none",ne(s.perPage*s.totalPages,s.perPage,s.page),O(s)}J();B();const z=new y;M();F();Q();document.querySelector(".product_card-list").addEventListener("click",K);X();z.PopularList.addEventListener("click",ee);V();B();J();z.discountList.addEventListener("click",U);ie.addEventListener("click",W);q.addEventListener("input",oe);re.addEventListener("click",le);G.addEventListener("submit",R);
+`).join("")}async function Z(){try{const e=k;return S.PopularList.insertAdjacentHTML("beforeend",X(e)),d=new y,C(),await e}catch(e){console.log(e.message)}}function C(){let e=L.loadCart();const a=[];"products"in e&&(e=e.products,e.forEach(t=>a.push(t.productId))),k.forEach(t=>{a.includes(t._id)?(d[`btn1${t._id}`].style.display="none",d[`check${t._id}`].style.display="flex"):(d[`check${t._id}`].style.display="none",d[`btn1${t._id}`].style.display="flex")})}const ee=document.getElementById("pagination");function te(e,a,t){const s={totalItems:e,itemsPerPage:a,visiblePages:5,page:t,centerAlign:!0};new U(ee,s).on("afterMove",ae)}async function ae(e){const a=e.page;let t=JSON.parse(localStorage.getItem("keyword")),s=JSON.parse(localStorage.getItem("category")),n={};var i=JSON.parse(localStorage.getItem("filters"))||{};i.page=a,t==""?t=null:t=JSON.parse(localStorage.getItem("keyword")),t==null?s.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?n={page:i.page}:n={category:s.replace(" ","_").replace("/","&").replace(" ","_"),page:i.page}:s.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?n={keyword:t,page:i.page}:n={category:s.replace(" ","_").replace("/","&").replace(" ","_"),keyword:t,page:i.page},console.log(n);const p=await m.products(n);return E(p)}const u=document.querySelector("#dropdownMenu"),O=document.querySelector("#input-filter"),se=document.querySelector("#filterButt"),f=document.querySelector(".category-content"),ne=document.querySelector(".dropdown-toggle"),v="keyword",A="category";let o=JSON.parse(localStorage.getItem(v)),r=JSON.parse(localStorage.getItem(A)),g={};function M(){(o||r)&&(O.value=o,f.textContent=r)}async function q(){let a=(await m.categories()).map(t=>t.replace("_"," ").replace("&","/").replace("_"," "));a.push("Show all"),r!==null?f.textContent=r:f.textContent="Categories",u.innerHTML="",a.forEach(async t=>{const s=document.createElement("div");s.className="dropdown-item",s.textContent=t,s.onclick=async function(){r=t,localStorage.setItem(A,JSON.stringify(r)),H()},u.appendChild(s)})}async function H(){u.style.display=u.style.display==="flex"?"none":"flex",r!==null?f.textContent=r:f.textContent="Categories",setTimeout(function(){u.style.opacity=u.style.opacity==="1"?"0":"1"},10)}function ce(e){const a=e.target.value;a?localStorage.setItem(v,JSON.stringify(a)):localStorage.setItem(v,JSON.stringify(null))}async function re(e){e.preventDefault(),o==""?o=null:o=JSON.parse(localStorage.getItem(v));let a=window.innerWidth||document.documentElement.clientWidth,t="";a>=1440?t=9:a<1440&&a>768?t=8:t=6,o==null?r.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?g={limit:t}:g={category:r.replace(" ","_").replace("/","&").replace(" ","_"),limit:t}:r.replace(" ","_").replace("/","&").replace(" ","_")=="Show_all"?g={keyword:o,limit:t}:g={category:r.replace(" ","_").replace("/","&").replace(" ","_"),keyword:o,limit:t};const s=await m.products(g);return s.results.length===0?I.style.display="flex":I.style.display="none",te(s.perPage*s.totalPages,s.perPage,s.page),E(s)}M();q();const J=new y;T();W();R();document.querySelector(".product_card-list").addEventListener("click",G);Y();J.PopularList.addEventListener("click",Q);z();q();M();J.discountList.addEventListener("click",D);ne.addEventListener("click",H);O.addEventListener("input",ce);se.addEventListener("click",re);F.addEventListener("submit",V);
 //# sourceMappingURL=commonHelpers2.js.map
